f1875205504867040194b224d6d6605e1ac90e74
A P note <b@b.b>
2020-01-12 23:13:24 +0200

    implement search functionality staying in movies/:searchPhrase route

--- src/app/movies/movie-list/movie-list.component.ts
+++ src/app/movies/movie-list/movie-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { from } from 'rxjs';
-import { RouterModule, ActivatedRoute, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { ApiService } from '../../api.service';
 
 @Component({
@@ -13,17 +13,36 @@ export class MovieListComponent implements OnInit {
     data;
     searchPhrase = '';
     constructor (private apiService: ApiService,
-                private route: ActivatedRoute) { }
+                private route: ActivatedRoute,
+                private router: Router) { }
     ngOnInit() {
         this.searchPhrase = this.route.snapshot.paramMap.get('searchPhrase');
+        this.searchUsingApiService(this.searchPhrase);
 
-        this.apiService.search(this.searchPhrase)
+        /*
+        router.events subscription covers the case when searching for a phrase
+        from within the same route movies/:searchPhrase
+        */
+        this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe(
+            (val) => {
+                this.route.params.subscribe((params) => {
+                    if (params['searchPhrase']) {
+                        this.searchUsingApiService(params['searchPhrase']);
+                    }
+                });
+            }
+        );
+    }
+
+    searchUsingApiService(phrase) {
+        this.searchPhrase = phrase;
+        this.apiService.search(phrase)
         // Subscribe to begin listening for async result
         .subscribe((data)=>{
                 this.data = data['Search'];
             }
         );
-
     }
-
 }
